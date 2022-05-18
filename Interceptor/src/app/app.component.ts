a3698b849306b00a1df3c1b2d827c723228a2887
imran2197 <imran251099@gmail.com>
2022-05-18 18:49:22 +0530

    User Idle Session Timeout

--- Interceptor/src/app/app.component.ts
+++ Interceptor/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UsersService } from './services/users.service';
+import { UserIdleService } from 'angular-user-idle';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,28 @@ import { UsersService } from './services/users.service';
 })
 export class AppComponent {
   title = 'Interceptor';
-  constructor(){}
+  sessionTime: number = 60;
+  timeCount: number | undefined;
 
-  ngOnInit(){}
+  constructor(private userIdle: UserIdleService, 
+    private router: Router){}
+
+  ngOnInit(){
+    this.sessionCheck();
+  }
+
+  sessionCheck() {
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd) {
+        this.userIdle.startWatching();
+        this.userIdle.onTimerStart().subscribe((count) => {
+          this.timeCount = this.sessionTime - count;
+          if(this.timeCount === 0){
+            this.userIdle.stopTimer();
+          }
+        });
+      }
+    })
+  }
   
 }
--- Interceptor/src/app/app.module.ts
+++ Interceptor/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersComponent } from './components/users/users.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { UserIdleModule } from 'angular-user-idle';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PostsComponent } from './components/posts/posts.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserIdleModule.forRoot({idle: 2, timeout: 300, ping: 120})
   ],
   providers: [
     {
